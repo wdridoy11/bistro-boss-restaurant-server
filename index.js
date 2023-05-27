94992519b5a85a8b39ea14f6411c0deb7664f5c4
Ridoy Sharif <developersridoy@gmail.com>
2023-05-27 09:46:55 +0600

    mongodb data loaded api created

--- index.js
+++ index.js
@@ -24,13 +24,21 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+     client.connect();
+    // 
+    const menuCollection = client.db("bistro_bossDB").collection("menu")
+
+    app.get("/menu",async(req,res)=>{
+      const result = await menuCollection.find().toArray();
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
