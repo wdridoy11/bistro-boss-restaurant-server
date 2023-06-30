6ab60e7124cbe08fcbe2ee6952b2f1311c8a92f3
Ridoy Sharif <developersridoy@gmail.com>
2023-06-30 18:39:17 +0600

    create user and send data mongodb database

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v2v9b72.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,8 +27,17 @@ async function run() {
      client.connect();
     // 
     const menuCollection = client.db("bistro_bossDB").collection("menu");
+    const usersCollection = client.db("bistro_bossDB").collection("users");
     const cartsCollection = client.db("bistro_bossDB").collection("carts");
     const reviewsCollection = client.db("bistro_bossDB").collection("reviews");
+
+
+    // create user and send data mongodb database
+    app.post("/users",async(req,res)=>{
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    })
 
     // menus item get from database
     app.get("/menus",async(req,res)=>{
@@ -42,13 +51,32 @@ async function run() {
       res.send(result);
     })
 
+    // get carts using email
+    app.get("/carts",async (req,res)=>{
+      const email = req.query.email;
+      if(!email){
+        res.send([])
+      }else{
+        const query = {email: email}
+        const result = await cartsCollection.find(query).toArray();
+        res.send(result)
+      }
+    })
+
     // menus carts added post data
     app.post("/carts",async(req,res)=>{
       const item = req.body;
       const result = await cartsCollection.insertOne(item)
       res.send(result)
     });
-    
+
+    app.delete("/carts/:id",async(req,res)=>{
+      const id = req.params.id;
+      const query ={_id : new ObjectId(id)}
+      const result = await cartsCollection.deleteOne(query);
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
