6aaea01f10d6178836a1b7fc97c7e4949638abb4
Ridoy Sharif <developersridoy@gmail.com>
2023-07-01 10:10:49 +0600

    jwt access token verify

--- index.js
+++ index.js
@@ -8,6 +8,26 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT =(req,res,next)=>{
+    const authorization = req.headers.authorization;
+    if(!authorization){
+        return res.status(401).send({error: true, message:"unauthorization access"})
+    }
+    // bearer token
+    const token = authorization.split(' ')[1];
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+      if(err){
+        return res.status(401).send({error: true, message:"unauthorization access"})
+      }
+      req.decoded = decoded;
+      next()
+    })
+}
+
+
+
+
+
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v2v9b72.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -24,7 +44,7 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
      client.connect();
-    // 
+
     const menuCollection = client.db("bistro_bossDB").collection("menu");
     const usersCollection = client.db("bistro_bossDB").collection("users");
     const cartsCollection = client.db("bistro_bossDB").collection("carts");
@@ -89,15 +109,18 @@ async function run() {
     })
 
     // get carts apis using email
-    app.get("/carts",async (req,res)=>{
+    app.get("/carts", verifyJWT, async (req,res)=>{
       const email = req.query.email;
       if(!email){
         res.send([])
-      }else{
+      }
+      const decodedEmail = req.decoded.email;
+      if(email !== decodedEmail){
+        return res.status(403).send({error: true, message:"unauthorization access"})
+      }
         const query = {email: email}
         const result = await cartsCollection.find(query).toArray();
         res.send(result)
-      }
     })
 
     // menus carts apis added post data
