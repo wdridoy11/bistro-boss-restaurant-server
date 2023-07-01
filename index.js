776d3eddf7ecc59d1be15105df733c09362f89e3
Ridoy Sharif <developersridoy@gmail.com>
2023-07-01 08:26:55 +0600

    users admin system added

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const express = require("express");
-require('dotenv').config();
 const app= express();
+require('dotenv').config();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
-
 app.use(cors());
 app.use(express.json());
-
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v2v9b72.mongodb.net/?retryWrites=true&w=majority`;
@@ -31,27 +29,50 @@ async function run() {
     const cartsCollection = client.db("bistro_bossDB").collection("carts");
     const reviewsCollection = client.db("bistro_bossDB").collection("reviews");
 
-
-    // create user and send data mongodb database
+    // 
+    app.get("/users",async(req,res)=>{
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    })
+    
+    // create user apis and send data mongodb database
     app.post("/users",async(req,res)=>{
       const user = req.body;
+      const query = {email: user.email}
+      const existingUser = await usersCollection.findOne(query);
+      if(existingUser){
+        return res.send({message:"user already exists"})
+      }
       const result = await usersCollection.insertOne(user);
       res.send(result);
     })
 
-    // menus item get from database
+    // create admin using id 
+    app.patch("/users/admin/:id",async(req,res)=>{
+      const id = req.params.id;
+      console.log(id)
+      const filter = {_id : new ObjectId(id)}
+      const updateDoc={
+        $set:{
+          role:"admin"
+        }
+      }
+      const result = await usersCollection.updateOne(filter,updateDoc);
+      res.send(result)
+    })
+    // menus item get apis from database
     app.get("/menus",async(req,res)=>{
       const result = await menuCollection.find().toArray();
       res.send(result);
     })
 
-    // user reviews data get from database
+    // user reviews apis data get from database
     app.get("/reviews",async(req,res)=>{
       const result = await reviewsCollection.find().toArray();
       res.send(result);
     })
 
-    // get carts using email
+    // get carts apis using email
     app.get("/carts",async (req,res)=>{
       const email = req.query.email;
       if(!email){
@@ -63,13 +84,14 @@ async function run() {
       }
     })
 
-    // menus carts added post data
+    // menus carts apis added post data
     app.post("/carts",async(req,res)=>{
       const item = req.body;
       const result = await cartsCollection.insertOne(item)
       res.send(result)
     });
 
+    // delete carts menu apis from database
     app.delete("/carts/:id",async(req,res)=>{
       const id = req.params.id;
       const query ={_id : new ObjectId(id)}
